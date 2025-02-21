15a52a86fa4acb820dba2ab67088b6580b122e02
AJ <admin@admins-MacBook-Air.local>
2025-02-21 08:37:57 +0000

    updated

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score == 5000) {
-            System.out.println("Your score was 5000");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score is less than 5000 and greater than 1000");
+        } else if (score < 1000){
+            System.out.println("Your score is less than 1000");
+        } else {
+            System.out.println("Got here");
         }
     }
 }

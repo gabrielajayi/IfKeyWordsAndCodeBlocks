9a146c34354952fe5c46b8a2f0fe00030adf7187
AJ <admin@admins-MacBook-Air.local>
2025-02-21 09:32:05 +0000

    methods tutorial

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -1,27 +1,55 @@
 public class MainChallenge {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted = 8;
-        int bonus = 200;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
         int finalScore = score;
+
+        calculateScore();
 
         if (gameOver == true) {
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
 
-        boolean newGameOver = true;
-        int newScore = 12000;
-        int newLevelCompleted = 12;
-        int newBonus = 1200;
+//        boolean newGameOver = true;
+//        int newScore = 12000;
+//        int newLevelCompleted = 12;
+//        int newBonus = 1200;
+//
+//        int newFinalScore = newScore;
+//
+//        if (newGameOver == true) {
+//            newFinalScore += (newLevelCompleted * newBonus);
+//            System.out.println("Your final score was " + newFinalScore);
+//        }
 
-        int newFinalScore = newScore;
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
 
-        if (newGameOver == true) {
-            newFinalScore += (newLevelCompleted * newBonus);
-            System.out.println("Your final score was " + newFinalScore);
+        finalScore = score;
+
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 1800;
+        int levelCompleted = 15;
+        int bonus = 1100;
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }

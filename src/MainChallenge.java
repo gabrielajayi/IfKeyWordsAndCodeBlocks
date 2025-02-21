2aa84e7c1fc0373998e5459982c1d6009110fd1e
AJ <admin@admins-MacBook-Air.local>
2025-02-21 09:01:43 +0000

    initial commit

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -5,11 +5,23 @@ public class MainChallenge {
         int levelCompleted = 8;
         int bonus = 200;
 
-       int finalScore = score;
+        int finalScore = score;
 
-       if (gameOver == true) {
-           finalScore += (levelCompleted * bonus);
-           System.out.println("Your final score was " + finalScore);
-       }
+        if (gameOver == true) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        boolean newGameOver = true;
+        int newScore = 12000;
+        int newLevelCompleted = 12;
+        int newBonus = 1200;
+
+        int newFinalScore = newScore;
+
+        if (newGameOver == true) {
+            newFinalScore += (newLevelCompleted * newBonus);
+            System.out.println("Your final score was " + newFinalScore);
+        }
     }
 }
